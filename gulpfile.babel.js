30f7d8117e225522cd775867b3afa7275f3ba3a4
Jonty Lee <ljlbj@foxmail.com>
2017-09-29 22:32:56 +0800

    add html watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -50,15 +50,40 @@ gulp.task('build-css', () => {
         .pipe(gulp.dest(dest + '/css'));
 });
 
-gulp.task('server', ['build-css', 'imgmin'], () => {
+gulp.task('html', () => {
+    return gulp.src(src + '/views/**/*.html')
+        .pipe($.useref())
+        // Minify any HTML
+        .pipe($.if('*.html', $.htmlmin({
+            removeComments: true,
+            collapseWhitespace: true,
+            collapseBooleanAttributes: true,
+            removeAttributeQuotes: true,
+            removeRedundantAttributes: true,
+            removeEmptyAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            removeOptionalTags: true
+        })))
+        // Output files
+        .pipe($.if('*.html', $.size({
+            title: 'html',
+            showFiles: true
+        })))
+        .pipe(gulp.dest(dest + '../application/views'));
+});
+
+
+gulp.task('server', ['build-css', 'imgmin', 'html'], () => {
     browserSync({
         notify: false,
         // Customize the Browsersync console logging prefix
-        logPrefix: 'NINGHAO',
+        logPrefix: 'Jonty',
         // Allow scroll syncing across breakpoints
         scrollElementMapping: ['main', '.mdl-layout'],
         proxy: 'yaf/'
     });
     gulp.watch([src + '/css/*'], ['build-css', reload]);
     gulp.watch([src + '/images/*'], ['imgmin', reload]);
+    gulp.watch([src + '/views/**/*.html'], ['html', reload]);
 });
